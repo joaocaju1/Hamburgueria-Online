b0f3e8f2b74bf112ce1e60c3a6a4925abe1125b2
joao guilherme pretti genari <jgpretti2002@gmail.com>
2023-06-28 17:59:30 -0300

    Projeto inteiro

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <router-link to="/" id="logo-url">
+      <img :src="logo" :alt="alt" id="logo">
+    </router-link>
+    <router-link to="/"><strong>Home</strong></router-link> |
+    <router-link to="/pedidos"><strong>Pedidos</strong></router-link>
+  </nav>
+</template>
+
+
+<script>
+
+export default {
+ name: "Navbar",
+  props: ["logo", "alt"]
+
+  }
+</script>
+
+<style >
+ nav{
+   background-color: #222;
+   border-bottom: 4px solid #111;
+   padding: 15px 50px;
+   display: flex;
+   justify-content: flex-end;
+   align-items: center;
+ }
+
+ #logo-url{
+   margin: auto;
+   margin-left: 0;
+ }
+
+ #logo{
+   width: 40px;
+   height: 40px;
+ }
+
+ nav a{
+   color: #FCBA03;
+   text-decoration: none;
+   margin: 12px;
+ }
+
+ nav a:hover {
+   color: #FFF;
+
+ }
+</style>
